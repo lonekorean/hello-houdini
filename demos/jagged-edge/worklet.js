febdaa3cb539e52aefb05ae0a5b4b43c94bd67be
Will Boyd <will@codersblock.com>
2018-02-18 16:51:04 -0500

    Fine tuning jagged edge demo

--- demos/jagged-edge/worklet.js
+++ demos/jagged-edge/worklet.js
@@ -1,18 +1,19 @@
 class JaggedEdgePainter {
     paint(ctx, size, props) {
-        let toothWidth = 300;
-        let toothHeight = 100;
+        let toothWidth = 40;
+        let toothHeight = 20;
 
         // lots of math to ensure teeth are collectively centered
         let spaceBeforeCenterTooth = (size.width - toothWidth) / 2;
         let teethBeforeCenterTooth = Math.ceil(spaceBeforeCenterTooth / toothWidth);
-        let startingPoint = spaceBeforeCenterTooth - teethBeforeCenterTooth * toothWidth;
+        let startX = spaceBeforeCenterTooth - teethBeforeCenterTooth * toothWidth;
         let totalTeeth = teethBeforeCenterTooth * 2 + 1;
 
-        console.log(startingPoint);
-
+        // draw each tooth
         for (let i = 0; i < totalTeeth; i++) {
-            let x = startingPoint + toothWidth * i;
+            let x = startX + toothWidth * i;
+
+            // draws triangle from bottom left to top to bottom right
             ctx.beginPath();
             ctx.moveTo(x, toothHeight);
             ctx.lineTo(x + toothWidth / 2, 0);
@@ -20,6 +21,9 @@ class JaggedEdgePainter {
             ctx.closePath();
             ctx.fill();
         }
+
+        // fill area below teeth
+        ctx.fillRect(0, toothHeight, size.width, size.height);
     }
 }
 
