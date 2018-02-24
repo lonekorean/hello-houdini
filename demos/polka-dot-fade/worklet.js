957c7d1f32a89eb0b7d332b9f0331436be662e0e
Will Boyd <will@codersblock.com>
2018-02-24 12:21:55 -0500

    Simplify polka dots demo

--- demos/polka-dot-fade/worklet.js
+++ demos/polka-dot-fade/worklet.js
@@ -7,32 +7,21 @@ class PolkaDotFadePainter {
         let dotSpacing = props.get('--dot-spacing').value;
         let dotFadeOffset = props.get('--dot-fade-offset').value;
 
-        let startX = this.startOffset(size.width, dotSpacing);
-        let stopX = size.width + dotSpacing;
-        let startY = this.startOffset(size.height, dotSpacing);
-        let stopY = size.height + dotSpacing;
+        for (let y = 0; y < size.height + dotSpacing; y += dotSpacing) {
+            for (let x = 0; x < size.width + dotSpacing; x += dotSpacing * 2) {
+                // every other row shifts x to create staggered dots
+                let staggerX = x + ((y / dotSpacing) % 2 === 1 ? dotSpacing : 0);
 
-        let isStaggerRow = false;
-        for (let y = startY; y < stopY; y += dotSpacing) {
-            for (let x = startX; x < stopX; x += dotSpacing * 2) {
-                let staggerX = x + (isStaggerRow ? dotSpacing : 0);
-
-                let sizingX = staggerX - size.width * dotFadeOffset / 100;
-                let radius = dotSpacing * Math.max(Math.min(1 - sizingX / size.width, 1), 0);
+                // calculate dot radius based on horizontal position and fade offset
+                let fadeRelativeX = staggerX - size.width * dotFadeOffset / 100;
+                let radius = dotSpacing * Math.max(Math.min(1 - fadeRelativeX / size.width, 1), 0);
                 
+                // draw dot
                 ctx.beginPath();
                 ctx.arc(staggerX, y, radius, 0, 2 * Math.PI);
                 ctx.fill();
             }
-            isStaggerRow = !isStaggerRow;
         }
-    }
-
-    startOffset(length, dotSpacing) {
-        let spaceBeforeCenterDot = length / 2;
-        let dotsBeforeCenterDot = Math.ceil(spaceBeforeCenterDot / dotSpacing);
-        let startOffset = spaceBeforeCenterDot - dotsBeforeCenterDot * dotSpacing;
-        return startOffset;
     }
 }
 
