172edd36300502c058de5fff94bfb474c180e94a
Will Boyd <will@codersblock.com>
2018-02-24 11:22:11 -0500

    Finally got the fade working

--- demos/polka-dot-fade/worklet.js
+++ demos/polka-dot-fade/worklet.js
@@ -1,45 +1,27 @@
 class PolkaDotFadePainter {
     static get inputProperties() {
-        return ['--dot-radius', '--dot-spacing', '--dot-zoom-radius', '--dot-swipe-position'];
+        return ['--dot-spacing', '--dot-fade-offset'];
     }
 
     paint(ctx, size, props) {
-        let dotRadius = props.get('--dot-radius').value;
         let dotSpacing = props.get('--dot-spacing').value;
-        let dotZoomRadius = props.get('--dot-zoom-radius').value;
-        let dotSwipePosition = props.get('--dot-swipe-position').value;
-
-        ctx.fillStyle = '#999';
+        let dotFadeOffset = props.get('--dot-fade-offset').value;
 
         let startX = this.startOffset(size.width, dotSpacing);
-        let stopX = size.width + dotSpacing + dotRadius;
+        let stopX = size.width + dotSpacing;
         let startY = this.startOffset(size.height, dotSpacing);
-        let stopY = size.height + dotSpacing + dotRadius;
+        let stopY = size.height + dotSpacing;
 
         let isStaggerRow = false;
         for (let y = startY; y < stopY; y += dotSpacing) {
             for (let x = startX; x < stopX; x += dotSpacing * 2) {
                 let staggerX = x + (isStaggerRow ? dotSpacing : 0);
 
-                let fadeDistance = 300;
-                let swipeX = dotSwipePosition / 100 * size.width;
-                let calcRadius;
-                if (swipeX > staggerX + fadeDistance) {
-                    // swipe is beyond this dot
-                    calcRadius = dotZoomRadius;
-                } else if (swipeX < staggerX - fadeDistance) {
-                    // swipe is before dot
-                    calcRadius = dotRadius
-                } else {
-                    // within fade range
-                    let zoomRange = dotZoomRadius - dotRadius;
-                    let per = (swipeX - staggerX) / (fadeDistance * 2);
-                    let adjust = zoomRange * per;
-                    calcRadius = dotZoomRadius + adjust;
-                }
-
+                let sizingX = staggerX - size.width * dotFadeOffset / 100;
+                let radius = dotSpacing * Math.max(Math.min(1 - sizingX / size.width, 1), 0);
+                
                 ctx.beginPath();
-                ctx.arc(staggerX, y, calcRadius, 0, 2 * Math.PI);
+                ctx.arc(staggerX, y, radius, 0, 2 * Math.PI);
                 ctx.fill();
             }
             isStaggerRow = !isStaggerRow;
